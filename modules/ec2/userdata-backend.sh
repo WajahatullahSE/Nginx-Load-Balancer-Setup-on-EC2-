1f5512e59aa5b995d4de14f41c1896e3675456b3
Wajahat Ullah <wajahatullahh21@gmail.com>
2025-11-13 20:58:47 +0500

    fixed script

--- modules/ec2/userdata-backend.sh
+++ modules/ec2/userdata-backend.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sleep 20    #Delay until the network configuration is done
+sudo systemctl disable packagekit
+sudo systemctl stop packagekit  
 yum update -y
+sleep w0  
 amazon-linux-extras install -y nginx1  
 systemctl start nginx
 systemctl enable nginx
